791f5d995e7745dc6017457984182209580a3f25
Peter De Clercq <dclercqpeter@gmail.com>
2021-05-04 12:07:59 +0200

    Registration page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Login from './pages/Login';
+import Registration from './pages/Registration';
 import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
-      <ResetPassword />
+      {/* <ResetPassword /> */}
+      <Registration />
     </div>
   );
 }
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -7,7 +7,7 @@ import FormLink from '../components/AccountForms/FormLink';
 import FormButton from '../components/AccountForms/FormButton';
 import FormLabelInput from '../components/AccountForms/FormLabelInput';
 
-const Login = () => {
+const Registration = () => {
   return (
     <div className={AccFormCSS.page_container}>
       <FormHeader />
@@ -16,15 +16,26 @@ const Login = () => {
           <img src={blueBlob} alt="Blue Blob" />
         </div>
         <div className={AccFormCSS.form_wrapper}>
-          <h1>Login</h1>
+          <h1>Registreer je account</h1>
+          <p>
+            Het BUFFL dashboard geeft een duidelijk & gestructureerd beeld van
+            je BUFFL <br /> campagne weer. De resultaten worden continu
+            vernieuwd en maken het <br /> mogelijk je campagne op de voet te
+            volgen.
+          </p>
           <form className={AccFormCSS.form} action="">
             <FormLabelInput label="E-mail" type="email" />
             <br />
             <br />
             <FormLabelInput label="Wachtwoord" type="password" />
-            <FormButton text="Login" act={() => console.log('login')} />
-            <FormLink text="Nog geen account aangemaakt? Registreer" link="@" />
-            <FormLink text="Je wachtwoord vergeten? Reset het hier" link="@" />
+            <br />
+            <br />
+            <FormLabelInput label="Herhaal wachtwoord" type="password" />
+            <FormButton
+              text="Registreer"
+              act={() => console.log('registreer')}
+            />
+            <FormLink text="Reeds een account aangemaakt? Login" link="@" />
           </form>
         </div>
         <div className={AccFormCSS.blob}>
@@ -35,4 +46,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Registration;
